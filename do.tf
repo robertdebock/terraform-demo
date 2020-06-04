421b7511edb70cb1324810a744e3b8828f90a97f
Robert de Bock <robert@meinit.nl>
2020-06-04 12:06:41 +0200

    Multiple resources.

--- do.tf
+++ do.tf
@@ -10,3 +10,30 @@ resource "digitalocean_droplet" "web-1" {
   region = "ams3"
   size   = "s-1vcpu-1gb"
 }
+
+resource "digitalocean_droplet" "web-2" {
+  image  = "ubuntu-18-04-x64"
+  name   = "web-2"
+  region = "ams3"
+  size   = "s-1vcpu-1gb"
+}
+
+resource "digitalocean_loadbalancer" "web" {
+  name   = "web"
+  region = "ams3"
+
+  forwarding_rule {
+    entry_port     = 80
+    entry_protocol = "http"
+
+    target_port     = 80
+    target_protocol = "http"
+  }
+
+  healthcheck {
+    port     = 22
+    protocol = "tcp"
+  }
+
+  droplet_ids = [digitalocean_droplet.web-1.id, digitalocean_droplet.web-2.id,]
+}
