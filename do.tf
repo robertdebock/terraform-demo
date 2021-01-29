a742ffcd83a70965e73ef2455fe26965ee2f8d9e
Robert de Bock <robert@meinit.nl>
2021-01-29 15:51:59 +0100

    Ready.

--- do.tf
+++ do.tf
@@ -10,3 +10,12 @@ resource "digitalocean_droplet" "web-1" {
   region = "ams3"
   size   = "s-1vcpu-1gb"
 }
+
+resource "local_file" "ssh_config" {
+  content  = templatefile("./templates/ssh_config.tpl",
+  {
+    ip       = digitalocean_droplet.web-1.ipv4_address,
+    username = "root"
+  } )
+  filename = "./ssh_config"
+}
---
+++ versions.tf
@@ -0,0 +1,13 @@
+terraform {
+  required_version = ">= 0.13"
+  required_providers {
+    digitalocean = {
+      source = "digitalocean/digitalocean"
+      version = "2.2.0"
+    }
+    local = {
+      source = "hashicorp/local"
+      version = "2.0.0"
+    }
+  }
+}
