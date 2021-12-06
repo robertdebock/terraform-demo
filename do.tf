49992709004473ba218efb2fa69308fbf4e412ce
Robert de Bock <robert@meinit.nl>
2021-12-06 15:48:29 +0100

    Minimum.

--- do.tf
+++ do.tf
@@ -1,21 +1,37 @@
-variable "do_token" {}
+terraform {
+  required_providers {
+    digitalocean = {
+      source = "digitalocean/digitalocean"
+      version = "2.16.0"
+    }
+  }
+}
 
 provider "digitalocean" {
   token = var.do_token
 }
 
+variable "do_token" {}
+
+resource "digitalocean_ssh_key" "default" {
+  name       = "Terraform demo"
+  public_key = file("/Users/robertdb/.ssh/id_rsa.pub")
+}
+
 resource "digitalocean_droplet" "web-1" {
-  image  = "fedora-32-x64"
+  image  = "fedora-35-x64"
   name   = "web-1"
   region = "ams3"
   size   = "s-1vcpu-1gb"
+  ssh_keys = [digitalocean_ssh_key.default.fingerprint]
 }
 
 resource "digitalocean_droplet" "web-2" {
-  image  = "fedora-32-x64"
+  image  = "fedora-35-x64"
   name   = "web-2"
   region = "ams3"
   size   = "s-1vcpu-1gb"
+  ssh_keys = [digitalocean_ssh_key.default.fingerprint]
 }
 
 resource "digitalocean_loadbalancer" "web" {
